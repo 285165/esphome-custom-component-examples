32911ae3e4a30ee5233bc6999ae9148e2c65d039
285165 <zsb@me.com>
2024-09-26 14:29:05 +0200

    Update __init__.py

--- esphome/components/gps/__init__.py
+++ esphome/components/gps/__init__.py
@@ -94,4 +94,4 @@ async def to_code(config):
         cg.add(var.set_satellites_sensor(sens))
 
     # https://platformio.org/lib/show/1655/TinyGPSPlus
-    cg.add_library("mikalhart/TinyGPSPlus", "1.0.2")
+    cg.add_library("mikalhart/TinyGPSPlus", "1.0.3a")
